b94cddfa8c1ee39f865940c2fe4f307830739c91
Jamal Zado <jamamilo12@gmail.com>
2026-09-15 12:51:57 +0200

    Add files via upload

---
+++ Uppgift 11.py
@@ -0,0 +1,12 @@
+
+Frukter = [ "äpple", "banan", "päron" ]
+print (Frukter)
+
+Ny_frukt = input(" Vilken frukt vill du lägga till i listan? ")
+Frukter.append(Ny_frukt)
+print(Frukter)
+
+Ny_listan = input(" Vilken frukt vill du ta bort från listan? ")
+Frukter.remove(Ny_listan)
+
+print(Frukter)
