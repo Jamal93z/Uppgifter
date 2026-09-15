4c8d4aff008427cfde0d5053f6c4828b6508e902
Jamal Zado <jamamilo12@gmail.com>
2026-09-15 13:07:27 +0200

    Uppgift 11.py

--- Uppgift 11.py
+++ Uppgift 11.py
@@ -1,12 +1,17 @@
 
 Frukter = [ "äpple", "banan", "päron" ]
-print (Frukter)
+print ("Här är våren lista med frukter:", Frukter)
+
 
 Ny_frukt = input(" Vilken frukt vill du lägga till i listan? ")
-Frukter.append(Ny_frukt)
-print(Frukter)
+while Ny_frukt in Frukter:
+    print("Denna frukt finns redan i listan.")
+    Ny_frukt = input(" Välj en annan frukt: ")
+else:    
+     Frukter.append(Ny_frukt)
+     print("Här är din uppdaterade lista med frukter:", Frukter)
 
-Ny_listan = input(" Vilken frukt vill du ta bort från listan? ")
+Ny_listan = input("Vilken frukt vill du ta bort från listan? ")
 Frukter.remove(Ny_listan)
 
-print(Frukter)
+print("Här är din uppdaterade lista med frukter:", Frukter)
